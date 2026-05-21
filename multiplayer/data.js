df82375e5707d48e8b96c824a73e05bd3f755988
Roblexxxz <daniela4354@mytusd.org>
2026-05-21 10:39:16 -0700

    Update data.js

--- multiplayer/data.js
+++ multiplayer/data.js
@@ -1,1 +1,7 @@
 localstorage = currentuser
+ currentuser.localstorage.value = name
+currentuser.localstorage.value = data
+const data = localstorage.data
+const name = localstorage.currentuser
+savedata.currentuser.data
+loaddata.name.data
